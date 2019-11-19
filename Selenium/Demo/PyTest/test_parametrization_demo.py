736c7d659cf2ab53f8a3a15155b35189532de18f
anasansari <anas.ansari@shahisawari.co>
2019-11-19 10:18:23 +0500

    Adding overall Requirements.txt and others libs

--- Selenium/Demo/PyTest/test_parametrization_demo.py
+++ Selenium/Demo/PyTest/test_parametrization_demo.py
@@ -5,25 +5,35 @@
 # To generate / get self contained Html reports:[Use to send the reports with style.css file in it]
 # In terminal / cmd give command     dirctory>pytest --html=./reports/report1.html --self-contained-html
 
+# ================================================
+#  For Allure reports:
+# 1) pip install allure-pytest
+# 2) pytest --alluredir=folder directory(relative or absolute path)   # to generate reports
+# 3) Install Java, nodejs and npm
+# 4) Run this command just one time : directory>npm install -g allure-commandline --save-dev
+# Then allure serve will work
+# 5) allure serve directory
+
 
 
 import pytest
 
+
 def sum(a, b):
     return a + b
 
+
 # Parametrise and take values(inputs and assertion) in the Tuple
 @pytest.mark.parametrize("input1, input2, output",
-    [
-        (3,5,8),
-        (5,6,11),
-        (9,9,20)
-    ]
-)
+                         [
+                             (3, 5, 8),
+                             (5, 6, 11),
+                             (9, 9, 20)
+                         ]
+                         )
 def test_calc_sum_1(input1, input2, output):
     result = sum(input1, input2)
     assert result == output
-
 
 # def test_calc_sum_2():
 #     result = sum(3, 3)
